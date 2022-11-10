32cc99008a85f52cdeb47868e32a0f99b51ea90a
liyuqing_551 <liyuqing_551@sjtu.edu.cn>
2022-11-10 17:09:36 +0800

    For test

--- Lie_Version2.m
+++ Lie_Version2.m
@@ -2,18 +2,19 @@ clear
 clc
 close all
 % Define the data
-r1 = sym([ 0 1 ]');
+r1 = sym([ -0.8 0.6 ]');
  
 r2 = sym([  0.6 -0.8 ]');
 %%
-documentation=1:6
-for N = 1:6
+test_time=4
+documentation=1:test_time
+for N = 1:test_time
 %% Set Hyper-Parameter
     Width = 2
 % Define the symbolic variables
  
     ad_fng = sym('ad_fng','real');
-    A = sym('A%d%d',[N+4 Width]);
+    A = sym('A%d%d',[N+2 Width]);
 %% Data Inner Product(内积线性相加)
     Dot_Data1=A*r1;
     Dot_Data2=A*r2;
@@ -26,11 +27,11 @@ for N = 1:6
     g= Output_Data2*r2';
     g=g(:) ;
     AA=A(:) ;
-    n =16;
+    n =8;
  
 % Call the Lie Bracket Function
     ad_fng=liebracket(f,g,AA,n);
-    D=[f,g,ad_fng(:,2),ad_fng(:,3),ad_fng(:,4),ad_fng(:,5),ad_fng(:,6),ad_fng(:,7),ad_fng(:,8),ad_fng(:,9),ad_fng(:,10),ad_fng(:,11),ad_fng(:,12),ad_fng(:,13),ad_fng(:,14),ad_fng(:,15)];
+    D=[f,g,ad_fng(:,2),ad_fng(:,3),ad_fng(:,4),ad_fng(:,5),ad_fng(:,6),ad_fng(:,7),ad_fng(:,8)]%,ad_fng(:,9),ad_fng(:,10)]%,ad_fng(:,11),ad_fng(:,12),ad_fng(:,13),ad_fng(:,14),ad_fng(:,15)];
     documentation(N)=rank(D)
 end    
 %D=[f(:),g(:),ad_fng(:,2)]
