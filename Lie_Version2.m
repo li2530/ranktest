ba17d5b1e16c77710549dc6618b475c4804f3e6a
liyuqing_551 <liyuqing_551@sjtu.edu.cn>
2022-11-21 06:34:02 +0800

    final vesrion

--- Lie_Version2.m
+++ Lie_Version2.m
@@ -2,11 +2,11 @@ clear
 clc
 close all
 % Define the data
-r1 = sym([ -0.5 0.4 ]');
+r1 = sym([ 1.0 0 ]');
  
-r2 = sym([  0.3 -0.1 ]');
+r2 = sym([  0.6  0.8 ]');
 %%
-test_time=5
+test_time=1
 documentation=1:test_time
 for N = 1:test_time
 %% Set Hyper-Parameter
@@ -14,10 +14,10 @@ for N = 1:test_time
 % Define the symbolic variables
  
     ad_fng = sym('ad_fng','real');
-    A = sym('A%d%d',[N+30 Width]);
+    A = sym('A%d%d',[N+10 Width]);
 %% Data Inner Product(内积线性相加)
-    Dot_Data1=A*r1;
-    Dot_Data2=A*r2;
+   % Dot_Data1=A*r1;
+   % Dot_Data2=A*r2;
 %% Put Sigma(激活函数)
     Output_Data1=exp(A*r1);%%%%%平方
     Output_Data2=exp(A*r2);
@@ -27,12 +27,12 @@ for N = 1:test_time
     g= Output_Data2*r2';
     g=g(:) ;
     AA=A(:) ;
-    n =40;
+    n=100;
  
 % Call the Lie Bracket Function
     ad_fng=liebracket(f,g,AA,n);
     ad_gnf=liebracket(g,f,AA,n);
-    D=[f,g,ad_fng(:,:),ad_gnf];%ad_fng(:,3),ad_fng(:,4),ad_fng(:,5),ad_fng(:,6),ad_fng(:,7),ad_fng(:,8)]%,ad_fng(:,9),ad_fng(:,10)]%,ad_fng(:,11),ad_fng(:,12),ad_fng(:,13),ad_fng(:,14),ad_fng(:,15)];
+    D=[f,g,ad_fng(:,:),ad_gnf(:,:)];%ad_fng(:,3),ad_fng(:,4),ad_fng(:,5),ad_fng(:,6),ad_fng(:,7),ad_fng(:,8)]%,ad_fng(:,9),ad_fng(:,10)]%,ad_fng(:,11),ad_fng(:,12),ad_fng(:,13),ad_fng(:,14),ad_fng(:,15)];
     documentation(N)=rank(D);
 end    
 %D=[f(:),g(:),ad_fng(:,2)]
--- wisTWO.m
+++ wisTWO.m
@@ -2,18 +2,16 @@ clear
 clc
 close all
 % Define the data
-r1 = sym([0.8 0.6 0 ]');
+r1 = sym([0.8 0.6 ]');
  
-r2 = sym([0.0 0.8 0.6 ]');
+r2 = sym([ 1.0 0 ]');
  
-
-r3 = sym([0.0 -0.8 0.6 ]');
 %%
-test_time=1
+test_time=3
 documentation=1:test_time
 for N = 1:test_time
 %% Set Hyper-Parameter
-    Width = 3
+    Width = 2
 % Define the symbolic variables
  
     ad_fng = sym('ad_fng','real');
@@ -23,16 +21,16 @@ for N = 1:test_time
  
     Data1=exp(W*r1);
     Data2=exp(W*r2);
-    Data3=exp(W*r3);
+     
  
 %% Data Inner Product(内积线性相加) 带有一阶导数
     Dot_Data1=exp(W*r1);
     Dot_Data2=exp(W*r2);
-    Dot_Data3=exp(W*r3);
+    
 %% Put Sigma(激活函数)带有一阶导数
     Output_Data1=A.*Dot_Data1; 
     Output_Data2=A.*Dot_Data2;
-    Output_Data3=A.*Dot_Data3;
+    
 %% Tensor Product
     f=Output_Data1*r1';
     ff=[Data1,f];
@@ -41,23 +39,20 @@ for N = 1:test_time
     g= Output_Data2*r2';
     gg=[Data2,g]
     g=gg(:) ;
-    
-    h= Output_Data3*r3';
-    hh=[Data3,h]
-    h=hh(:) ;
+   
     
     WW=W(:);
     AA=A(:) ;
     SYM=[A,W];
     SYM=SYM(:);
-    n =3;
+    n =4;
  
 % Call the Lie Bracket Function
     ad_fng=liebracket(f,g,SYM,n);
     %ad_fnh=liebracket(f,h,SYM,n);
     %ad_gnh=liebracket(g,h,SYM,n);
     ad_gnf=liebracket(g,f,SYM,n);
-    %ad_hnf=liebracket(h,f,SYM,n);
+    %ad_hnf=liebracket(h,f,SYM,n);r
     %ad_hng=liebracket(h,g,SYM,n);
     %D=[f,g,h,ad_fng(:,:),ad_fnh(:,:),ad_gnh(:,:),ad_gnf(:,:),ad_hnf(:,:),ad_hng(:,:)];
     D=[f,g,ad_fng(:,:),ad_gnf(:,:)];%ad_fng(:,3),ad_fng(:,4),ad_fng(:,5),ad_fng(:,6),ad_fng(:,7),ad_fng(:,8)]%,ad_fng(:,9),ad_fng(:,10)]%,ad_fng(:,11),ad_fng(:,12),ad_fng(:,13),ad_fng(:,14),ad_fng(:,15)];
